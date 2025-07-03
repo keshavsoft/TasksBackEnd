e7f1b60b4c50aeb24ae37783372b89441761f8d1
keshavsoft <nknnkumar@yahoo.com>
2025-07-03 18:36:44 +0530

    app changed

--- app.js
+++ app.js
@@ -4,18 +4,14 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const port = 3000;
 
-import { StartFunc as StartFuncFromMiddleware } from "./Token/MiddleWares/entryFile.js";
-
 import { router as routerFromToken } from "./Token/routes.js";
 import { router as routerFromV1 } from "./V1/routes.js";
-import { router as routerFromSV1 } from "./SV1/routes.js";
 
 app.use(express.static('public'));
 app.use(cookieParser());
 
 app.use("/Token", routerFromToken);
 app.use("/V1", routerFromV1);
-app.use("/SV1", StartFuncFromMiddleware, routerFromSV1);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
