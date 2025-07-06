c0d4a725eec99e46ea65054e593ec3a74c9cfd70
keshavsoft <nknnkumar@yahoo.com>
2025-07-06 18:10:10 +0530

    crud good

--- Public/V2/Tasks/Js/SubTable/Crud/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/TableFooterSaveButtonId/FetchFromFuncs/PostFetch.js
+++ Public/V2/Tasks/Js/SubTable/Crud/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/TableFooterSaveButtonId/FetchFromFuncs/PostFetch.js
@@ -1,9 +1,11 @@
 import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js";
 import UrlJson from "./url.json" with { type: "json" };
 import ConfigJson from "../../../../../../../../Config.json" with { type: "json" };
+
 let StartFunc = async ({ inCurrentTarget }) => {
     let jVarTableName = ConfigJson.TableName;
     let LocalroutePath = UrlJson.PostendPoint;
+
     let jVarLocalFetchHeaders = StartFuncFetchHeaders({ inCurrentTarget });
     let jVarLocalFetchUrl = `${jVarTableName}/${LocalroutePath}`;
     let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
